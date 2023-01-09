d13e32719481eabf42a253d7f2995ca0e4e7bf0f
Milosz Gembicki <mg1313@live.mdx.ac.uk>
2023-01-09 18:26:13 +0000

    added routes with lists of either lessons and user

--- server.js
+++ server.js
@@ -9,13 +9,29 @@ app.use(express.static("public"));
 //Status codes defined in external file
 require('./http_status.js');
 
-app.get("/lessons", lessons); //
+app.get('/lessons', (req, res) => {
+    const lessons = [
+        { topic: 'math', location: 'hendon', price: 100 },
+        { topic: 'math', location: 'colindale', price: 80 },
+        { topic: 'math', location: 'brent cross', price: 90 },
+        { topic: 'math', location: 'golders green', price: 120 },
+    ];
+    res.json(lessons);
+});
+
+app.get('/user', (req, res) => {
+    const user = {
+        email: 'user@email.com',
+        password: 'mypassword',
+    };
+    res.json(user);
+});
 
 async function lessons(request, response) {
 
     response.json(data);
 
-    };
+};
 
 
 
