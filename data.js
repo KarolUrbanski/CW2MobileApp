784827a18c65ca0679bd096bf45027047f5821d0
Karol <60113497+Majcioch1@users.noreply.github.com>
2023-01-11 10:17:24 +0100

    Delete data.js

--- data.js
+++
@@ -1,1 +0,0 @@
-[{"topic":"math","location":"hendon","price":100},{"topic":"math","location":"colindale","price":80},{"topic":"math","location":"brent cross","price":90},{"topic":"math","location":"golders green","price":120}]
